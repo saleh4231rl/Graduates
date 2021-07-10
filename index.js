4f28f8a9f9e7185ac760c63630f47f6eb7b1e044
saleh4231rl <52216220+saleh4231rl@users.noreply.github.com>
2021-07-10 20:50:57 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+var nama="Andi";
+var nilai=50;
+
+if(nilai<=100 && nilai>=80){
+    console.log("Selamat Kamu Mendapat Nilai "+nilai+", Grademu A")
+} 
+else if(nilai<=79 && nilai>=65){
+    console.log("Selamat Kamu Mendapat Nilai "+nilai+", Grademu B")
+}
+else if(nilai<=64 && nilai>=50){
+    console.log("Selamat Kamu Mendapat Nilai "+nilai+", Grademu C")
+}
+else if(nilai<=49 && nilai>=35){
+    console.log("Selamat Kamu Mendapat Nilai "+nilai+", Grademu D")
+}
+else if(nilai<=34 && nilai>=0){
+    console.log("Selamat Kamu Mendapat Nilai "+nilai+", Grademu E")
+}
+else{
+    console.log("Nilai Invalid")
+}
+// Insert your code here
+
+// index
+// var i = kata.length-1; i>=0; i--
